b284119f2624d38d641944cfe2ed2a33246ac38c
Tom Dörr <tom.doerr@tum.de>
2021-08-25 01:10:03 +0200

    Update file

--- save_env_runs.py
+++ save_env_runs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import gym
+import time
 
 log_file='data/rollouts.txt'
 
@@ -14,10 +15,12 @@ def reward_to_str(obs):
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     env = gym.make('CartPole-v0')
-    for i in range(int(1e6)):
+    for episode in range(int(1e9)):
         observation = env.reset()
         episode_str = ''
+        reward_sum = 0
         for _ in range(100):
             # env.render()
             action = env.action_space.sample()
@@ -26,12 +29,25 @@ if __name__ == '__main__':
 
 
             observation, reward, done, _ = env.step(action) # take a random action
+            reward_sum += reward
 
+            # episode_str += f'{obs_str} reward_sum: xxx reward: {reward}  action: {action}   '
             episode_str += f'{obs_str} reward: {reward}  action: {action}   '
+            if done:
+                break
 
-        print("episode_str:", episode_str)
-        with open(log_file, 'a') as f:
-            f.write(episode_str + '\n')
+        if False:
+            episode_str = episode_str.replace('xxx', str(int(reward_sum)))
+        else:
+            episode_str = f'reward_sum: {int(reward_sum)} ' + episode_str
+
+        episodes_per_second = episode / (time.time() - start_time)
+        if episode % 2000 == 0:
+            print("episodes_per_second:", episodes_per_second)
+        # print("episode_str:", episode_str)
+        if reward_sum > 80:
+            with open(log_file, 'a') as f:
+                f.write(episode_str + '\n')
 
 
     env.close()
